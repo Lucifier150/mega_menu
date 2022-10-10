62f8cdf426779597ac751c4c2d9c98269464d85d
Anas Ali Khan <105873373+Lucifier150@users.noreply.github.com>
2022-10-10 13:47:16 +0500

    Task Complete

--- index.js
+++ index.js
@@ -1,17 +1,84 @@
-var i = 0;
+
 
 function menu() {
     if (i == 0) {
-        // document.querySelector(".navigation_dropdown-active").style.display = "block"
-        document.querySelector(".navigation_dropdown-active").style.top = "0"
-        document.querySelector(".navigation_dropdown-active").style.opacity = "1"
+        document.querySelector(".responsive-dropdown").style.display = "block"
         i++;
     } else {
-        // document.querySelector(".navigation_dropdown-active").style.display = "none"
-        document.querySelector(".navigation_dropdown-active").style.top = "-38rem"
-        document.querySelector(".navigation_dropdown-active").style.opacity = "0"
+        document.querySelector(".responsive-dropdown").style.display = "none"
         i--;
 
     }
 
 }
+let i = 0;
+const nav = document.querySelector(".horizontalnavigation");
+
+const dropdowns = document.querySelectorAll(".navdropdown");
+
+
+var navlinks = nav.querySelectorAll(".horzontalnavigation-list-item.active");
+// loop
+navlinks.forEach((element, i) => {
+
+
+    //Using arrow function.
+
+    let flag = true
+    element.onclick = () => {
+        // switch (i) {
+        // case 0:
+        dropdowns.forEach(element => {
+            if(flag){
+
+                element.style.top = "-200rem";        
+                flag = true
+            }
+            
+        });
+        if (flag) {
+            dropdowns[i].style.top = "0";
+            flag = false
+
+        }
+        else {
+            dropdowns[i].style.top = "-200rem";
+            flag = true
+        }
+        //         break;
+        //     case 1:
+        //         if (flag) {
+        //             feature.style.top = "0";
+        //             flag = false
+
+        //         }
+        //         else {
+        //             feature.style.top = "-50rem";
+        //             flag = true
+        //         }
+        //         break;
+        //     case 2:
+
+        //         break;
+        //     default:
+
+        // }
+
+        // console.log(flag)
+        // console.log(element,i)
+
+        // if (flag) {
+        //     active.style.top = "0";
+        //     flag = false
+
+        // }
+        // else {
+        //     active.style.top = "-50rem";
+        //     flag = true
+        // }
+    }
+});
+
+
+
+
